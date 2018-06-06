6ee56e1a0d7babba2b397e48ff6cb4d7db8d7e35
Nils Tekampe <nt@konfidas.de>
2018-06-06 15:44:07 +0200

    developing install.sh further

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 
+if ! [ -x "$(command -v git)" ]; then
+  echo 'Error: git is not installed.' >&2
+  exit 1
+fi
+
+git clone https://github.com/nils-tekampe/dotfiles.git ~/.dotfiles
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/tmux.conf ~/tmux.conf
